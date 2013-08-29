a20c5713f1c2a4395f21dda4e6f7577386bdfd4a
Rick Cerf <rickcerf@gmail.com>
2013-08-29 16:10:13 -0700

    testing for arrays and objects with loops inside of tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,47 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+
+	var stringifiedObj = "";
+	var stringify = function(item){
+		stringifiedObj = stringifiedObj+", "+item;
+	}
+	//Test if obj is a string
+	if(typeof(obj) === "string"){
+		stringify(obj);
+	}
+	//Test if obj is Array
+	if(obj instanceof Array){
+		//pull out elements from array
+		for(var i=0; i<obj.length; i++){
+			//TEST: if element is empty
+			if(obj[i].length === 0){
+				stringify("[]");
+			//TEST: if element is an object
+			}else if(obj[i] instanceof Object){
+				//recurse
+				stringifyJSON(obj[i]);
+			}else{
+				stringify(obj[i]);
+			}
+		}
+	}
+	//If obj is Object
+	if(obj instanceof Object){
+		//Pull out 1 element from object
+		for(var property in obj){
+			//test if object is empty
+			}if(obj[property].length === 0){
+				stringify("{}");
+			//TEST: if element is embeded object
+			}else if(obj[property] instanceof Object){
+				//recurse
+				stringifyJSON(obj[property]);
+			}else{
+				//stringify element
+				stringify(obj[property]);
+			}
+		}
+	}
+	console.log(stringifiedObj);
 };
