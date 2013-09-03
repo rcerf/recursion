2860c21fcff3114c696b4057bf9169aa21022c85
Rick Cerf <rickcerf@gmail.com>
2013-09-02 20:33:19 -0700

    created internal partial array to handle object key value pairs. working on embedded object.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,43 +1,21 @@
-var each = function(collection, iterator) {
-  if(Array.isArray(collection)){
-    for(var i =0; i<collection.length; i++){
-      iterator(collection[i], i, collection);
-    }
-  } else {
-    for(var key in collection){
-      iterator(collection[key], key, collection);
-    }
-  }
-
-};
-
-// Return the results of applying an iterator to each element.
-var map = function(array, iterator) {
-  // map() is a useful primitive iteration function that works a lot
-  // like each(), but in addition to running the operation on all
-  // the members, it also maintains an array of results.
-  var results = [];
-  each(array, function(value, index){
-    var result = iterator(value);
-    results.push(result);
-    console.log("result returned " + result);
-  });
-  
-  return results;
-};
 
 //Recursive stringify function
 var stringifyJSON = function(obj){
+  var partial,
+      i,
+      k,
+      v;
 
 	// if bool, string, num, null, undefined
 	// return '' + obj (string version)
-	if(typeof(obj) === "number" || typeof(obj) === "boolean" || obj == undefined){
+	if(typeof(obj) === "number" || typeof(obj) === "boolean" || obj === null){
       console.log("primitive " + obj);
   		return String(obj);
 	} 
-  else if(typeof(obj) === "string"){
+ 
+ if(typeof(obj) === "string"){
     //maintain the printing of the quotation marks for strings.
-    // *** Look into espcaping rules ***
+    console.log(obj);
     return "\"" + obj + "\"" ;
   }
 
@@ -45,19 +23,39 @@ var stringifyJSON = function(obj){
   // return map of each element passed to stringify
   // ex: [1,2,3] return [1,2,3].map(function(el){ return stringify(el)}).join(', ')
   // return result of map
-  else if(obj instanceof Array){
-    return ("[" + map(obj, function(item){
-      console.log("array " + item);
-      return stringifyJSON(item);
-    }).join(",") + "]");
-  } 
+  if(obj instanceof Object){
 
-  // return: handle objects too, again values recursive
+    //Make an array to handle partial results
+    partial = [];
 
-  else if(obj instanceof Object){
-    return "{" + map(obj, function(item, key){
-      return stringifyJSON(item);
-    }).join(",") + "}";
+    //handle arrays
+    if(obj instanceof Array){
+      //The value is an array. Stringify every element.
+      for(i=0; i<obj.length; i++){
+        partial[i] = stringifyJSON(obj[i]);
+      }
+
+      //Join all the elements together, seperate with commas, and wrap them in brackets.
+      v = partial.length === 0
+        ? "[]"
+        : "[" + partial.join(",") + "]";
+
+      return v;
+    }else{
+    // handle objects
+
+      for(key in obj){
+        v = stringifyJSON(obj[key]);
+        k = stringifyJSON(key);
+        partial.push(k + ":" + v);
+      }
+    }
+    
+    v = partial.length === 0
+      ? "{}"
+      : "{" + partial.join(",") + "}";
+
+    return v;
   }
 
 }
